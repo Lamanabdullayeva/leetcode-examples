54760a5fa3f14360322191d46d661c852532dc8c
Ləman Abdullayeva <la.abdullayeva99@gmail.com>
2025-03-04 15:13:15 +0000

    code makeup

--- src/problems/arrays/container_with_most_area.ts
+++ src/problems/arrays/container_with_most_area.ts
@@ -7,10 +7,10 @@ function maxArea(height: number[]): number {
     while (left < right) {
         let area = Math.min(height[left], height[right]) * (right - left);
         max = Math.max(max, area)
-        if (height[left] < height[right]){
-            left++
+        if (height[left] < height[right]) {
+            left++;
         } else {
-            right--
+            right--;
         }
     }
 
